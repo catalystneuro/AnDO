814db07cf01ff344b73d8f1098bcd229c24c68f5
Saksham Sharda <saksham20.sharda@gmail.com>
2021-04-10 20:52:09 +0530

    black formatting

--- ando/tools/generator/bidsconverter.py
+++ ando/tools/generator/bidsconverter.py
@@ -9,22 +9,24 @@ from tqdm import tqdm
 
 from ando.AnDOChecker import is_valid
 
-REQ_FILES = dict(description='dataset_description.json',
-                 participant='participants.tsv',
-                 session='{subject_label}\\{subject_label}_sessions.tsv',
-                 channels='{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_channels.tsv',
-                 contacts='{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_contacts.tsv',
-                 ephys='{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_ephys.json',
-                 probes='{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_probes.tsv',
-                 file='{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_ephys{file_type}')
+REQ_FILES = dict(
+    description="dataset_description.json",
+    participant="participants.tsv",
+    session="{subject_label}\\{subject_label}_sessions.tsv",
+    channels="{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_channels.tsv",
+    contacts="{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_contacts.tsv",
+    ephys="{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_ephys.json",
+    probes="{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_probes.tsv",
+    file="{subject_label}\\{session_label}\\ephys\\{subject_label}_{session_label}_ephys{file_type}",
+)
 
 
 class BidsConverter(ABC):
-    file_type = ''
+    file_type = ""
 
     def __init__(self, dataset_path, **kwargs):
         self.dataset_path = Path(dataset_path)
-        self.output_path = self.dataset_path.parent/'BIDSExt'/self.dataset_path.name
+        self.output_path = self.dataset_path.parent / "BIDSExt" / self.dataset_path.name
         self._kwargs = kwargs
         self._participants_dict = dict()
         self._dataset_desc_json = dict()
@@ -34,8 +36,8 @@ class BidsConverter(ABC):
         self._ephys_dict = defaultdict(dict)
         self._probes_dict = defaultdict(dict)
         self._file_name_dict = defaultdict(dict)
-        self.datafiles_list = list(self.dataset_path.glob(f'**/*{self.file_type}'))
-        assert len(self.datafiles_list) > 0, f'no files of type {self.file_type} found'
+        self.datafiles_list = list(self.dataset_path.glob(f"**/*{self.file_type}"))
+        assert len(self.datafiles_list) > 0, f"no files of type {self.file_type} found"
         self._datafiles_io = self._datafiles_open()
         self._datafiles = [file.read() for file in self._datafiles_io]
         self._labels_dict = defaultdict(list)
@@ -56,56 +58,92 @@ class BidsConverter(ABC):
 
     def _create_participant_df(self):
         participants_df = None
-        for file in self._tqdm(self._datafiles, 'participant'):
+        for file in self._tqdm(self._datafiles, "participant"):
             participant_df = self._get_participant_info(file)
-            sub_label = participant_df['ParticipantID'][0]
+            sub_label = participant_df["ParticipantID"][0]
             if sub_label not in self._labels_dict:
                 if participants_df is None:
                     participants_df = participant_df
                 else:
                     participants_df = participants_df.append(participant_df)
                 self._labels_dict[sub_label] = []
-        self._participants_dict = self._get_default_dict('participant', data=participants_df)['']
+        self._participants_dict = self._get_default_dict(
+            "participant", data=participants_df
+        )[""]
 
     def _create_description_json(self):
-        self._dataset_desc_json = self._get_default_dict('description',
-                                                         data=self._get_dataset_info(self._datafiles[0]))['']
+        self._dataset_desc_json = self._get_default_dict(
+            "description", data=self._get_dataset_info(self._datafiles[0])
+        )[""]
 
     def _create_session_df(self):
-        for file in self._tqdm(self._datafiles, 'session'):
+        for file in self._tqdm(self._datafiles, "session"):
             subject_name = self._get_subject_label(file)
             session_label = self._get_session_label(file)
             session_df = self._get_session_info(file)
-            sessions_df = self._sessions_dict.get(subject_name,
-                                                  dict(data=pd.DataFrame(
-                                                      columns=session_df.columns)))['data']
-            if not sessions_df['session_id'].str.contains(session_label).any():
+            sessions_df = self._sessions_dict.get(
+                subject_name, dict(data=pd.DataFrame(columns=session_df.columns))
+            )["data"]
+            if not sessions_df["session_id"].str.contains(session_label).any():
                 sessions_df = sessions_df.append(session_df)
                 self._labels_dict[subject_name].append(session_label)
             self._sessions_dict[subject_name] = self._get_default_dict(
-                'session', subject_name, session_label, data=sessions_df)[session_label]
+                "session", subject_name, session_label, data=sessions_df
+            )[session_label]
 
     def _create_sessionlevel_data(self):
-        for no, file in enumerate(self._tqdm(self._datafiles, 'sessionlevel')):
+        for no, file in enumerate(self._tqdm(self._datafiles, "sessionlevel")):
             subject_name = self._get_subject_label(file)
             session_label = self._get_session_label(file)
-            self._probes_dict[subject_name].update(self._get_default_dict(
-                'probes', subject_name, session_label, data=self._get_probes_info(file)))
-            self._ephys_dict[subject_name].update(self._get_default_dict(
-                'ephys', subject_name, session_label, data=self._get_ephys_info(file)))
-            self._channels_dict[subject_name].update(self._get_default_dict(
-                'channels', subject_name, session_label, data=self._get_channels_info(file)))
-            self._contacts_dict[subject_name].update(self._get_default_dict(
-                'contacts', subject_name, session_label, data=self._get_contacts_info(file)))
-            self._file_name_dict[subject_name].update(self._get_default_dict(
-                'file', subject_name, session_label, data=self.datafiles_list[no]))
+            self._probes_dict[subject_name].update(
+                self._get_default_dict(
+                    "probes",
+                    subject_name,
+                    session_label,
+                    data=self._get_probes_info(file),
+                )
+            )
+            self._ephys_dict[subject_name].update(
+                self._get_default_dict(
+                    "ephys",
+                    subject_name,
+                    session_label,
+                    data=self._get_ephys_info(file),
+                )
+            )
+            self._channels_dict[subject_name].update(
+                self._get_default_dict(
+                    "channels",
+                    subject_name,
+                    session_label,
+                    data=self._get_channels_info(file),
+                )
+            )
+            self._contacts_dict[subject_name].update(
+                self._get_default_dict(
+                    "contacts",
+                    subject_name,
+                    session_label,
+                    data=self._get_contacts_info(file),
+                )
+            )
+            self._file_name_dict[subject_name].update(
+                self._get_default_dict(
+                    "file", subject_name, session_label, data=self.datafiles_list[no]
+                )
+            )
 
-    def _get_default_dict(self, key, subject_label='', session_label='', data=None):
-        return {session_label: dict(
-            name=REQ_FILES[key].format(subject_label=subject_label,
-                                       session_label=session_label,
-                                       file_type=self.file_type),
-            data=data)}
+    def _get_default_dict(self, key, subject_label="", session_label="", data=None):
+        return {
+            session_label: dict(
+                name=REQ_FILES[key].format(
+                    subject_label=subject_label,
+                    session_label=session_label,
+                    file_type=self.file_type,
+                ),
+                data=data,
+            )
+        }
 
     @abstractmethod
     def _get_subject_label(self, file):
@@ -153,29 +191,31 @@ class BidsConverter(ABC):
         self._create_session_df()
         self._create_sessionlevel_data()
         self._create_description_json()
-        return dict(description=self._dataset_desc_json,
-                    sessions=self._sessions_dict,
-                    participant=self._participants_dict,
-                    probes=self._probes_dict,
-                    ephys=self._ephys_dict,
-                    channels=self._channels_dict,
-                    contacts=self._contacts_dict,
-                    file=self._file_name_dict)
+        return dict(
+            description=self._dataset_desc_json,
+            sessions=self._sessions_dict,
+            participant=self._participants_dict,
+            probes=self._probes_dict,
+            ephys=self._ephys_dict,
+            channels=self._channels_dict,
+            contacts=self._contacts_dict,
+            file=self._file_name_dict,
+        )
 
     @staticmethod
     def _write_tsv(data, write_path):
         write_path.parent.mkdir(parents=True, exist_ok=True)
-        assert isinstance(data, pd.DataFrame), f'{data} should be a df'
+        assert isinstance(data, pd.DataFrame), f"{data} should be a df"
         if not write_path.exists():
-            data.dropna(axis='columns', how='all', inplace=True)
-            data.to_csv(write_path, sep='\t', index=False)
+            data.dropna(axis="columns", how="all", inplace=True)
+            data.to_csv(write_path, sep="\t", index=False)
 
     @staticmethod
     def _write_json(data, write_path):
         write_path.parent.mkdir(parents=True, exist_ok=True)
-        assert isinstance(data, dict), f'{data} should be a dict'
+        assert isinstance(data, dict), f"{data} should be a dict"
         if not write_path.exists():
-            with open(write_path, 'w') as j:
+            with open(write_path, "w") as j:
                 json.dump(data, j)
 
     @staticmethod
@@ -188,96 +228,91 @@ class BidsConverter(ABC):
             if not dest.exists():
                 dest.symlink_to(source)
 
-    def organize(self, output_path=None, move_file=False,
-                 re_write=True, validate=True):
+    def organize(self, output_path=None, move_file=False, re_write=True, validate=True):
         if output_path is not None:
             self.output_path = Path(output_path)
         if re_write and self.output_path.exists():
             shutil.rmtree(self.output_path)
         self.output_path.mkdir(parents=True, exist_ok=True)
-        data, loc = self._parse_data_dict(
-            self._participants_dict,
-            self.output_path)
+        data, loc = self._parse_data_dict(self._participants_dict, self.output_path)
         self._write_tsv(data, loc)
-        data, loc = self._parse_data_dict(
-            self._dataset_desc_json,
-            self.output_path)
+        data, loc = self._parse_data_dict(self._dataset_desc_json, self.output_path)
         self._write_json(data, loc)
         for participant, sessions in self._labels_dict.items():
             data, loc = self._parse_data_dict(
-                self._sessions_dict[participant],
-                self.output_path)
+                self._sessions_dict[participant], self.output_path
+            )
             self._write_tsv(data, loc)
             for session in sessions:
                 data, loc = self._parse_data_dict(
-                    self._channels_dict[participant][session],
-                    self.output_path)
+                    self._channels_dict[participant][session], self.output_path
+                )
                 self._write_tsv(data, loc)
                 data, loc = self._parse_data_dict(
-                    self._probes_dict[participant][session],
-                    self.output_path)
+                    self._probes_dict[participant][session], self.output_path
+                )
                 self._write_tsv(data, loc)
                 data, loc = self._parse_data_dict(
-                    self._contacts_dict[participant][session],
-                    self.output_path)
+                    self._contacts_dict[participant][session], self.output_path
+                )
                 self._write_tsv(data, loc)
                 data, loc = self._parse_data_dict(
-                    self._ephys_dict[participant][session],
-                    self.output_path)
+                    self._ephys_dict[participant][session], self.output_path
+                )
                 self._write_json(data, loc)
                 data, loc = self._parse_data_dict(
-                    self._file_name_dict[participant][session],
-                    self.output_path)
+                    self._file_name_dict[participant][session], self.output_path
+                )
                 self._move_data_file(data, loc, move=move_file)
         if validate:
             is_valid(self.output_path)
 
     def _parse_data_dict(self, data_dict, output_path):
-        return data_dict['data'], output_path/data_dict['name']
+        return data_dict["data"], output_path / data_dict["name"]
 
     def get_subject_names(self):
-        return list(self._participants_dict['data']['ParticipantID'])
+        return list(self._participants_dict["data"]["ParticipantID"])
 
     def get_session_names(self, subject_name=None):
         if subject_name is None:
             subject_name = self.get_subject_names()[0]
-        return list(self._sessions_dict[subject_name]['data']['session_id'])
+        return list(self._sessions_dict[subject_name]["data"]["session_id"])
 
     def get_channels_info(self, subject_name=None, session_name=None):
         if subject_name is None:
             subject_name = self.get_subject_names()[0]
         if session_name is None:
             session_name = self.get_session_names()[0]
-        return self._channels_dict[subject_name][session_name]['data'].to_dict()
+        return self._channels_dict[subject_name][session_name]["data"].to_dict()
 
     def get_contacts_info(self, subject_name=None, session_name=None):
         if subject_name is None:
             subject_name = self.get_subject_names()[0]
         if session_name is None:
             session_name = self.get_session_names()[0]
-        return self._contacts_dict[subject_name][session_name]['data'].to_dict()
+        return self._contacts_dict[subject_name][session_name]["data"].to_dict()
 
     def get_ephys_info(self, subject_name=None, session_name=None):
         if subject_name is None:
             subject_name = self.get_subject_names()[0]
         if session_name is None:
             session_name = self.get_session_names()[0]
-        return self._ephys_dict[subject_name][session_name]['data']
+        return self._ephys_dict[subject_name][session_name]["data"]
 
     def get_probes_info(self, subject_name=None, session_name=None):
         if subject_name is None:
             subject_name = self.get_subject_names()[0]
         if session_name is None:
             session_name = self.get_session_names()[0]
-        return self._probes_dict[subject_name][session_name]['data'].to_dict()
+        return self._probes_dict[subject_name][session_name]["data"].to_dict()
 
     def get_participants_info(self):
-        return self._participants_dict['data'].to_dict()
+        return self._participants_dict["data"].to_dict()
 
     def get_dataset_description(self):
-        return self._dataset_desc_json['data']
+        return self._dataset_desc_json["data"]
 
     def get_session_info(self, subject_name=None):
         if subject_name is None:
             subject_name = self.get_subject_names()[0]
-        return self._sessions_dict[subject_name]['data'].to_dict()
+        return self._sessions_dict[subject_name]["data"].to_dict()
--- ando/tools/generator/nwb2bidsgenerator.py
+++ ando/tools/generator/nwb2bidsgenerator.py
@@ -8,42 +8,61 @@ from .bidsconverter import BidsConverter
 
 
 class NwbToBIDS(BidsConverter):
-    file_type = '.nwb'
+    file_type = ".nwb"
 
     def __init__(self, dataset_path, **kwargs):
         super().__init__(dataset_path, **kwargs)
 
     def _datafiles_open(self):
-        return [NWBHDF5IO(str(file), 'r') for file in self._tqdm(self.datafiles_list, 'reading nwbfiles')]
+        return [
+            NWBHDF5IO(str(file), "r")
+            for file in self._tqdm(self.datafiles_list, "reading nwbfiles")
+        ]
 
-    def _get_subject_label(self, file, subject_suffix=''):
+    def _get_subject_label(self, file, subject_suffix=""):
         if file.subject is not None:
             sb = file.subject
             if sb.subject_id is not None:
-                sub_id = re.sub(r'[\W_]+', '', sb.subject_id)
-                subject_label = f'sub-{sub_id}'
+                sub_id = re.sub(r"[\W_]+", "", sb.subject_id)
+                subject_label = f"sub-{sub_id}"
             else:
                 subject_label = f'sub-{sb.date_of_birth.strftime("%Y%m%dT%H%M")}'
         else:
-            subject_label = f'sub-noname{subject_suffix}'
+            subject_label = f"sub-noname{subject_suffix}"
         return subject_label
 
     def _get_session_label(self, file):
         if file.session_id is not None:
-            ses_id = re.sub(r'[\W_]+', '', file.session_id)
-            session_label = f'ses-{ses_id}'
+            ses_id = re.sub(r"[\W_]+", "", file.session_id)
+            session_label = f"ses-{ses_id}"
         else:
             session_label = f'ses-{file.session_start_time.strftime("%Y%m%dT%H%M")}'
         return session_label
 
-    def _get_participant_info(self, nwbfile, subject_suffix=''):
+    def _get_participant_info(self, nwbfile, subject_suffix=""):
         subject_label = self._get_subject_label(nwbfile, subject_suffix)
         participant_df = pd.DataFrame(
-            columns=['Species', 'ParticipantID', 'Sex', 'Birthdate', 'Age', 'Genotype', 'Weight'])
+            columns=[
+                "Species",
+                "ParticipantID",
+                "Sex",
+                "Birthdate",
+                "Age",
+                "Genotype",
+                "Weight",
+            ]
+        )
         if nwbfile.subject is not None:
             sb = nwbfile.subject
-            df_row = [sb.species, subject_label, sb.sex[0] if sb.sex is not None else None,
-                      sb.date_of_birth, sb.age, sb.genotype, sb.weight]
+            df_row = [
+                sb.species,
+                subject_label,
+                sb.sex[0] if sb.sex is not None else None,
+                sb.date_of_birth,
+                sb.age,
+                sb.genotype,
+                sb.weight,
+            ]
         else:
             df_row = [None, subject_label, None, None, None, None, None]
         participant_df.loc[0] = df_row
@@ -52,29 +71,38 @@ class NwbToBIDS(BidsConverter):
     @staticmethod
     def _get_dataset_info(nwbfile):
         return dict(
-            InstitutionName=nwbfile.institution, InstitutionalDepartmentName=nwbfile.lab,
-            Name='Electrophysiology', BIDSVersion='1.0.X',
-            Licence='CC BY 4.0',
+            InstitutionName=nwbfile.institution,
+            InstitutionalDepartmentName=nwbfile.lab,
+            Name="Electrophysiology",
+            BIDSVersion="1.0.X",
+            Licence="CC BY 4.0",
             Authors=[
-                list(nwbfile.experimenter) if nwbfile.experimenter is not None else None][0])
+                list(nwbfile.experimenter) if nwbfile.experimenter is not None else None
+            ][0],
+        )
 
     def _get_session_info(self, nwbfile):
-        trials_len = len(
-            nwbfile.trials) if nwbfile.trials is not None else None
+        trials_len = len(nwbfile.trials) if nwbfile.trials is not None else None
         session_label = self._get_session_label(nwbfile)
         session_df = pd.DataFrame(
-            columns=['session_id', '#_trials', 'comment'],
-            data=[[session_label, trials_len, nwbfile.session_description]])
+            columns=["session_id", "#_trials", "comment"],
+            data=[[session_label, trials_len, nwbfile.session_description]],
+        )
         return session_df
 
     @staticmethod
     def _get_channels_info(nwbfile):
         channels_df = pd.DataFrame(
-            columns=['channel_id', 'Contact_id', 'type', 'units', 'sampling_frequency',
-                     'unit_conversion_multiplier'])
-        es = [
-            i for i in nwbfile.children if isinstance(
-                i, ElectricalSeries)]
+            columns=[
+                "channel_id",
+                "Contact_id",
+                "type",
+                "units",
+                "sampling_frequency",
+                "unit_conversion_multiplier",
+            ]
+        )
+        es = [i for i in nwbfile.children if isinstance(i, ElectricalSeries)]
         if len(es) > 0:
             es = es[0]
             no_channels = es.data.shape[1]
@@ -82,42 +110,45 @@ class NwbToBIDS(BidsConverter):
             conversion = es.conversion
             unit = es.unit
             for chan_no in range(no_channels):
-                channels_df.loc[len(channels_df.index)] = [chan_no, chan_no, 'neural signal',
-                                                           unit,
-                                                           sampling_frequency, conversion]
+                channels_df.loc[len(channels_df.index)] = [
+                    chan_no,
+                    chan_no,
+                    "neural signal",
+                    unit,
+                    sampling_frequency,
+                    conversion,
+                ]
         return channels_df
 
     @staticmethod
     def _get_ephys_info(nwbfile, **kwargs):
-        return dict(PowerLineFrequency=kwargs.get('powerlinefrequency', 50.0))
+        return dict(PowerLineFrequency=kwargs.get("powerlinefrequency", 50.0))
 
     @staticmethod
     def _get_contacts_info(nwbfile):
         contacts_df = pd.DataFrame(
-            columns=[
-                'x',
-                'y',
-                'z',
-                'impedance',
-                'contact_id',
-                'probe_id',
-                'Location'])
+            columns=["x", "y", "z", "impedance", "contact_id", "probe_id", "Location"]
+        )
         e_table = nwbfile.electrodes
         if e_table is not None:
             for contact_no in range(len(e_table)):
-                contacts_df.loc[len(contacts_df.index)] = [e_table.x[contact_no],
-                                                           e_table.y[contact_no],
-                                                           e_table.z[contact_no],
-                                                           e_table.imp[contact_no],
-                                                           contact_no,
-                                                           e_table.group[contact_no].device.name,
-                                                           e_table.location[contact_no]]
+                contacts_df.loc[len(contacts_df.index)] = [
+                    e_table.x[contact_no],
+                    e_table.y[contact_no],
+                    e_table.z[contact_no],
+                    e_table.imp[contact_no],
+                    contact_no,
+                    e_table.group[contact_no].device.name,
+                    e_table.location[contact_no],
+                ]
         return contacts_df
 
     def _get_probes_info(self, nwbfile, **kwargs):
         contacts_df = self._get_contacts_info(nwbfile)
-        probes_df = pd.DataFrame(columns=['probeID', 'type'])
-        for probe_id in contacts_df['probe_id'].unique():
+        probes_df = pd.DataFrame(columns=["probeID", "type"])
+        for probe_id in contacts_df["probe_id"].unique():
             probes_df.loc[len(probes_df.index)] = [
-                probe_id, kwargs.get('probe_type', 'acute')]
+                probe_id,
+                kwargs.get("probe_type", "acute"),
+            ]
         return probes_df
